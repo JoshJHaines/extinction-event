4c39c034684c000c0236cb72ca5b8ec30c546cc0
JoshuaJHaines <joshjhaines3@gmail.com>
2021-03-18 10:06:50 -0400

    Part 3 comlpete...maybe. Not too clear in the instructions what effect we were looking to do.

--- main.js
+++ main.js
@@ -1,18 +1,29 @@
-const lineThruOL = document.querySelectorAll('ol');
+const lineThruOL = document.querySelectorAll("ol");
 
 for (const li of lineThruOL) {
-    li.addEventListener('click', function (event) {
-        console.log(event);
-        const elementThatWasClicked = event.target;
-        elementThatWasClicked.style.textDecoration = "line-through";
-    });
+	li.addEventListener("click", function (event) {
+		console.log(event);
+		const elementThatWasClicked = event.target;
+		elementThatWasClicked.style.textDecoration = "line-through";
+	});
 }
 
-const opacList = document.querySelectorAll('ul')
+const opacList = document.querySelectorAll("ul");
 
-for (const ul of opacList){
-    ul.addEventListener('click', function(event){
-        const elementThatWasClicked = event.target;
-        elementThatWasClicked.style.opacity = '0.25'
-    })
+for (const ul of opacList) {
+	ul.addEventListener("click", function (event) {
+		const elementThatWasClicked = event.target;
+		elementThatWasClicked.style.opacity = "0.25";
+	});
 }
+
+const pictures = document.querySelectorAll(".feature");
+
+for (const picture of pictures) {
+	picture.addEventListener("click", function (event) {
+		const elementThatWasClicked = event.target;
+		elementThatWasClicked.style.opacity = "0.15";
+	});
+}
+
+
